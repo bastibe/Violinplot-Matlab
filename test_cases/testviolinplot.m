cae9f95b1e0c59615d70c2c9e1d17bcb02513c57
binelp <binel@ipe.mavt.ethz.ch>
2022-03-21 12:32:56 +0100

    Adds two test cases

--- test_cases/testviolinplot.m
+++ test_cases/testviolinplot.m
@@ -1,6 +1,6 @@
 
 % TEST CASE 1
-display('Test 1: Violin plot default options');
+disp('Test 1: Violin plot default options');
 load carbig MPG Origin
 Origin = cellstr(Origin);
 figure
@@ -8,14 +8,40 @@ vs = violinplot(MPG, Origin);
 ylabel('Fuel Economy in MPG');
 xlim([0.5, 7.5]);
 
-display('Test 1 passed ok');
+disp('Test 1 passed ok');
 
 % TEST CASE 2
-display('Test 2: Test the plot ordering option');
+disp('Test 2: Test the plot ordering option');
 grouporder={'USA','Sweden','Japan','Italy','Germany','France','England'};
     
 figure;
 vs2 = violinplot(MPG,Origin,'GroupOrder',grouporder);
-display('Test 2 passed ok');
+disp('Test 2 passed ok');
+xlim([0.5, 7.5]);
+
+% TEST CASE 3
+disp('Test 3: Test the numeric input construction mode');
+figure
+cats = categorical(Origin);
+catnames = (unique(cats)); % this ignores categories without any data
+catnames_labels = {};
+thisData = NaN(length(MPG),length(catnames));
+for n = 1:length(catnames)
+    thisCat = catnames(n);
+    catnames_labels{n} = char(thisCat);
+    thisData(1:length(MPG(cats == thisCat)),n) = MPG(cats == thisCat);
+end
+vs3 = violinplot(thisData,catnames_labels);
+xlim([0.5, 7.5]);
+disp('Test 3 passed ok');
+
+% TEST CASE 4
+disp('Test 4: Test two sided violin plots. Japan is being compared.');
+figure
+C = colororder;
+vs4 = violinplot({thisData,repmat(thisData(:,5),1,7)},catnames_labels,'ViolinColor',{C,C(5,:)},'ViolinAlpha',{0.3 0.3});
+xlim([0.5, 7.5]);
+disp('Test 4 passed ok');
+
 
 %other test cases could be added here
